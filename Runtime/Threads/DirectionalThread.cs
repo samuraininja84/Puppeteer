c6d63fc628b2ec2716cc989298b2be5cb5284cf1
Samuraininja84 <158503209+samuraininja84@users.noreply.github.com>
2025-11-20 14:32:24 -0500

    Enhance Gizmo drawing with move direction and arrows
    Refactor Gizmo drawing logic to include move direction check and add DrawArrow class for arrow visualization.

--- Runtime/Threads/DirectionalThread.cs
+++ Runtime/Threads/DirectionalThread.cs
@@ -64,14 +64,18 @@ namespace Puppeteer
 
         protected virtual void OnDrawGizmosSelected()
         {
-            // Set Gizmo color
-            Gizmos.color = Color.cyan;
+            // Check if the enter interaction is null or if the move direction is zero
+            if (GetMoveDirection() != Vector2.zero)
+            {
+                // Get the position of the passage and the direction of the enter interaction
+                Vector3 pos = transform.position;
 
-            // Draw a line indicating the move direction
-            Gizmos.DrawLine(transform.position, transform.position + (Vector3)GetMoveDirection());
+                // Get the direction in 3D space
+                Vector3 direction = GetArrowDirection(GetMoveDirection(), Camera.main.orthographic);
 
-            // Draw a sphere at the end of the direction line
-            Gizmos.DrawWireSphere(transform.position + (Vector3)GetMoveDirection(), 0.1f);
+                // Draw the arrow
+                DrawArrow.ForGizmo(pos, direction, Color.green);
+            }
         }
 
         public enum Direction
@@ -83,5 +87,71 @@ namespace Puppeteer
             None
         }
     }
+
+    public static class DrawArrow
+    {
+        public static void ForGizmo(Vector3 pos, Vector3 direction, float arrowHeadLength = 0.25f, float arrowHeadAngle = 20.0f, float arrowPosition = 1f)
+        {
+            ForGizmo(pos, direction, Gizmos.color, arrowHeadLength, arrowHeadAngle, arrowPosition);
+        }
+
+        public static void ForGizmoTwoPoints(Vector3 from, Vector3 to, float arrowHeadLength = 0.25f, float arrowHeadAngle = 20.0f, float arrowPosition = 1f)
+        {
+            ForGizmoTwoPoints(from, to, Gizmos.color, arrowHeadLength, arrowHeadAngle, arrowPosition);
+        }
+
+        public static void ForGizmo(Vector3 pos, Vector3 direction, Color color, float arrowHeadLength = 0.25f, float arrowHeadAngle = 20.0f, float arrowPosition = 1f)
+        {
+            Gizmos.color = color;
+            Gizmos.DrawRay(pos, direction);
+            DrawArrowEnd(true, pos, direction, color, arrowHeadLength, arrowHeadAngle, arrowPosition);
+        }
+
+        public static void ForGizmoTwoPoints(Vector3 from, Vector3 to, Color color, float arrowHeadLength = 0.25f, float arrowHeadAngle = 20.0f, float arrowPosition = 1f)
+        {
+            Gizmos.DrawLine(from, to);
+            Vector3 direction = to - from;
+            DrawArrowEnd(true, from, direction, color, arrowHeadLength, arrowHeadAngle, arrowPosition);
+        }
+
+        public static void ForDebug(Vector3 pos, Vector3 direction, float arrowHeadLength = 0.25f, float arrowHeadAngle = 20.0f, float arrowPosition = 1f)
+        {
+            ForDebug(pos, direction, Color.white, arrowHeadLength, arrowHeadAngle, arrowPosition);
+        }
+
+        public static void ForDebug(Vector3 pos, Vector3 direction, Color color, float arrowHeadLength = 0.25f, float arrowHeadAngle = 20.0f, float arrowPosition = 1f)
+        {
+            Debug.DrawRay(pos, direction, color);
+            DrawArrowEnd(false, pos, direction, color, arrowHeadLength, arrowHeadAngle, arrowPosition);
+        }
+
+        private static void DrawArrowEnd(bool gizmos, Vector3 pos, Vector3 direction, Color color, float arrowHeadLength = 0.25f, float arrowHeadAngle = 20.0f, float arrowPosition = 1f)
+        {
+            Vector3 right = (Quaternion.LookRotation(direction) * Quaternion.Euler(arrowHeadAngle, 0, 0) * Vector3.back) * arrowHeadLength;
+            Vector3 left = (Quaternion.LookRotation(direction) * Quaternion.Euler(-arrowHeadAngle, 0, 0) * Vector3.back) * arrowHeadLength;
+            Vector3 up = (Quaternion.LookRotation(direction) * Quaternion.Euler(0, arrowHeadAngle, 0) * Vector3.back) * arrowHeadLength;
+            Vector3 down = (Quaternion.LookRotation(direction) * Quaternion.Euler(0, -arrowHeadAngle, 0) * Vector3.back) * arrowHeadLength;
+
+            Vector3 arrowTip = pos + (direction * arrowPosition);
+
+            if (gizmos)
+            {
+                Gizmos.color = color;
+                Gizmos.DrawRay(arrowTip, right);
+                Gizmos.DrawRay(arrowTip, left);
+                Gizmos.DrawRay(arrowTip, up);
+                Gizmos.DrawRay(arrowTip, down);
+            }
+            else
+            {
+                Debug.DrawRay(arrowTip, right, color);
+                Debug.DrawRay(arrowTip, left, color);
+                Debug.DrawRay(arrowTip, up, color);
+                Debug.DrawRay(arrowTip, down, color);
+            }
+        }
+    }
 }
+
+
 
